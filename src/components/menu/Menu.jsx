c3cd0444683e886e9f8a0b644b04ca0249e02d37
Bohdan Horetskyi <111376936+bhoretskyi@users.noreply.github.com>
2024-12-12 22:51:08 +0100

    add reverse functional

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,29 +1,80 @@
+import  { useState } from 'react';
 import eclipse from '../../img/eclipse.png';
 import menu from '../../img/menu.png';
 import verify from '../../img/verify.png';
 import star from '../../img/star.png';
 
-import { MenuSection } from './Menu.styled';
+import { MenuSection, MenuWrapper } from './Menu.styled';
 import { StarImg } from './Menu.styled';
 import { AvatarWrapper } from './Menu.styled';
+import { UserWrapper } from './Menu.styled';
+import { UserNameWrapper } from './Menu.styled';
+import { UserText } from './Menu.styled';
+import { GradientText } from './Menu.styled';
+import { UserInfo } from './Menu.styled';
 
 export const Menu = () => {
+  const [isReverse, setIsReverse] = useState(false);
+  const handleReverse = () => {
+    setIsReverse(prev => !prev);
+  };
   return (
     <>
-      <MenuSection>
-        <AvatarWrapper>
-          <img src={eclipse} alt="avatar" />
-          <StarImg src={star} alt="star" />
-        </AvatarWrapper>
-        <div>
-          <h2>Anna Effective</h2>
-          <img src={verify} alt="verify_mark" />{' '}
-          <p>Double your income with me!</p>
-        </div>
-        <div>
-          <img src={menu} alt="menu_btn" />
-        </div>
-      </MenuSection>
+      {isReverse ? (
+        <MenuSection>
+          <AvatarWrapper>
+            <img src={eclipse} alt="avatar" />
+            <StarImg src={star} alt="star" />
+          </AvatarWrapper>
+          <UserWrapper>
+            <UserInfo>
+              <UserNameWrapper>
+                {' '}
+                <h2>Anna Effective</h2>
+                <img
+                  src={verify}
+                  alt="verify_mark"
+                  width="60px"
+                  height="60px"
+                />{' '}
+              </UserNameWrapper>
+              <UserText>Double your income with me!</UserText>
+            </UserInfo>
+            <GradientText>4881</GradientText>
+          </UserWrapper>
+          <MenuWrapper>
+            <img src={menu} alt="menu_btn" />
+          </MenuWrapper>
+        </MenuSection>
+      ) : (
+        <MenuSection>
+          <MenuWrapper>
+            <img src={menu} alt="menu_btn" />
+          </MenuWrapper>
+          <UserWrapper>
+            <UserInfo>
+              <UserNameWrapper>
+                {' '}
+                <h2>Anna Effective</h2>
+                <img
+                  src={verify}
+                  alt="verify_mark"
+                  width="60px"
+                  height="60px"
+                />{' '}
+              </UserNameWrapper>
+              <UserText>Double your income with me!</UserText>
+            </UserInfo>
+            <GradientText>4881</GradientText>
+          </UserWrapper>
+
+          <AvatarWrapper>
+            <img src={eclipse} alt="avatar" />
+            <StarImg src={star} alt="star" />
+          </AvatarWrapper>
+        </MenuSection>
+      )}
+      <button onClick={handleReverse}> reverse</button>
     </>
   );
 };
--- src/components/menu/Menu.styled.js
+++ src/components/menu/Menu.styled.js
@@ -7,13 +7,43 @@ export const MenuSection = styled.section`
 `;
 
 export const AvatarWrapper = styled.div`
-position: relative;
-`
+  position: relative;
+`;
 export const StarImg = styled.img`
   position: absolute;
   right: 0;
   bottom: 10px;
-  border: 5px solid white; 
-  border-radius: 50%; 
-  
+  border: 5px solid white;
+  border-radius: 50%;
+`;
+export const UserInfo = styled.div`
+  padding-left: 50px;
+  margin-bottom: 30px;
+`;
+export const UserWrapper = styled.div`
+  margin-right: 150px;
+`;
+export const UserNameWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  font-size: 40px;
+  margin-bottom: 20px;
+`;
+export const UserText = styled.p`
+  font-size: 40px;
+`;
+export const GradientText = styled.p`
+  font-size: 35px;
+  font-weight: bold;
+  background: linear-gradient(to left, #68e0cf, #209cff);
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
+
+export const MenuWrapper = styled.div`
+  width: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
