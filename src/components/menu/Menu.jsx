1a182c849ea4137433e3daba07998e2c667512f8
Bohdan Horetskyi <111376936+bhoretskyi@users.noreply.github.com>
2024-12-12 21:46:56 +0100

    add avatar styles

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,9 +1,29 @@
+import eclipse from '../../img/eclipse.png';
+import menu from '../../img/menu.png';
+import verify from '../../img/verify.png';
+import star from '../../img/star.png';
+
+import { MenuSection } from './Menu.styled';
+import { StarImg } from './Menu.styled';
+import { AvatarWrapper } from './Menu.styled';
+
 export const Menu = () => {
-    return (<><section>
-        <div>avatar</div>
-        <div>text</div>
-        <div>menu</div>
-        </section></>
-       
-    )
-}
+  return (
+    <>
+      <MenuSection>
+        <AvatarWrapper>
+          <img src={eclipse} alt="avatar" />
+          <StarImg src={star} alt="star" />
+        </AvatarWrapper>
+        <div>
+          <h2>Anna Effective</h2>
+          <img src={verify} alt="verify_mark" />{' '}
+          <p>Double your income with me!</p>
+        </div>
+        <div>
+          <img src={menu} alt="menu_btn" />
+        </div>
+      </MenuSection>
+    </>
+  );
+};
---
+++ src/components/menu/Menu.styled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+export const MenuSection = styled.section`
+  display: flex;
+  padding-top: 50px;
+  padding-left: 20px;
+  padding-right: 20px;
+`;
+
+export const AvatarWrapper = styled.div`
+position: relative;
+`
+export const StarImg = styled.img`
+  position: absolute;
+  right: 0;
+  bottom: 10px;
+  border: 5px solid white; 
+  border-radius: 50%; 
+  
+`;
